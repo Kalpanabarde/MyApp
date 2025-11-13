1c43ecec3d178cee516b6b06ce91f48453abdcce
Kalpanabarde <98540032+Kalpanabarde@users.noreply.github.com>
2025-11-13 13:43:11 +0530

    Update welcome messages in index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,8 +18,8 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <ThemedText type="title">Hello World!</ThemedText>
+        <ThemedText type="title">Welcome to You!</ThemedText>
+        <ThemedText type="title">Hello World How are you !</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
